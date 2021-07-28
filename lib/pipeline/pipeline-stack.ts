abaa4e4b1043cfa32ceb2ecb967bcc0c50f8443e
Ralf Oliveira Ferreira <ralf.oliveira@inatel.br>
2021-07-28 11:00:55 -0300

    add: products function

--- lib/pipeline/pipeline-stack.ts
+++ lib/pipeline/pipeline-stack.ts
@@ -2,6 +2,7 @@ import * as codepipeline from "@aws-cdk/aws-codepipeline";
 import * as codepipeline_actions from "@aws-cdk/aws-codepipeline-actions";
 import * as cdk from "@aws-cdk/core";
 import { CdkPipeline, SimpleSynthAction } from "@aws-cdk/pipelines";
+import { ECommerceStage } from "./commerce-stage";
 
 export class PipelineStack extends cdk.Stack {
 	constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -36,6 +37,18 @@ export class PipelineStack extends cdk.Stack {
 			}),
 		});
 
-		// TODO: add pipeline stages
+		/**
+		 * Pipeline stages
+		 */
+		pipeline.addApplicationStage(
+			new ECommerceStage(this, "Stage1", {
+				// conta do deployment deste stage - não necessariamente igual da pipeline
+				// pode-se ter os stages (dev, qa, prod, ...) em contas diferentes
+				env: {
+					account: "743908444175",
+					region: "us-east-1",
+				},
+			})
+		);
 	}
 }
