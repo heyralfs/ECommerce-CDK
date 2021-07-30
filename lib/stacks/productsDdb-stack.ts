d27f655a0953096b283a8cb59825f3c29ee5e0de
Ralf Oliveira Ferreira <ralf.oliveira@inatel.br>
2021-07-30 11:07:12 -0300

    DDB auto scaling

--- lib/stacks/productsDdb-stack.ts
+++ lib/stacks/productsDdb-stack.ts
@@ -18,5 +18,31 @@ export class ProductsDdbStack extends cdk.Stack {
 			readCapacity: 1,
 			writeCapacity: 1,
 		});
+
+		/**
+		 * Auto scaling config
+		 */
+		const readScale = this.table.autoScaleReadCapacity({
+			maxCapacity: 4,
+			minCapacity: 1,
+		});
+		readScale.scaleOnUtilization({
+			// faz um upScaling se atingir 50% da capacidade de leitura
+			targetUtilizationPercent: 50,
+			// se fez um upScaling, não faz outro em menos de 60s
+			scaleInCooldown: cdk.Duration.seconds(60),
+			// se fez um downScaling, não faz outro em menos de 60s
+			scaleOutCooldown: cdk.Duration.seconds(60),
+		});
+
+		const writeScale = this.table.autoScaleWriteCapacity({
+			maxCapacity: 4,
+			minCapacity: 1,
+		});
+		writeScale.scaleOnUtilization({
+			targetUtilizationPercent: 50,
+			scaleInCooldown: cdk.Duration.seconds(60),
+			scaleOutCooldown: cdk.Duration.seconds(60),
+		});
 	}
 }
